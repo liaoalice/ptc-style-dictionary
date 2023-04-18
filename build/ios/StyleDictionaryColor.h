cc347cef3c9d6b0f978a8daa061060b95b256dba
Alice Liao <alliao@ptc.com>
2023-04-18 15:31:07 -0400

    Initial test commit

---
+++ build/ios/StyleDictionaryColor.h
@@ -0,0 +1,26 @@
+
+//
+// StyleDictionaryColor.h
+//
+
+// Do not edit directly
+// Generated on Tue, 18 Apr 2023 15:05:01 GMT
+
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorBaseGrayLight,
+ColorBaseGrayMedium,
+ColorBaseGrayDark,
+ColorBaseRed,
+ColorBaseGreen,
+ColorFontBase,
+ColorFontSecondary,
+ColorFontTertiary
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
